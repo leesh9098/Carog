22ccbe5df53bc99c29c915bf2e342f40944c3f36
LimHaeJun <6462jun@gmail.com>
2025-08-17 22:09:08 +0900

    정비내역 수정 기능 구현

--- src/pages/management/maintenance-history/id/index.tsx
+++ src/pages/management/maintenance-history/id/index.tsx
@@ -3,15 +3,67 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Calendar } from "@/components/ui/calendar"
 import React, { useState } from "react";
 import { ChevronDown } from "lucide-react";
+import { ax, getCookie } from "@/lib/utils";
+import { useSelectedCar } from "@/contexts/SelectedCarContext";
 
 export default function MaintenanceHistoryId() {
     const [date, setDate] = React.useState<Date | undefined>(new Date());
     const [isOpenCalendar, setIsOpenCalendar] = useState(false);
+    const [item, setItem] = useState("");
+    const [price, setPrice] = useState("");
+    const [mileage, setMileage] = useState("");
+    const [company, setCompany] = useState("");
+    const [memo, setMemo] = useState("");
     const navigate = useNavigate();
+    const { selectedCar } = useSelectedCar();
+    const { id } = useParams();
+
+    const handleGet = async () => {
+        const token = getCookie(`token`);
+        try {
+            await ax.get(`/maintenance/list/${selectedCar?.id}`, {
+                params: {
+                    carInfoId: selectedCar?.id,
+                    item,
+                    price,
+                    mileage,
+                    company,
+                    memo,
+                    date,
+                },
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function handleUpdate(){
+        const token = getCookie(`token`);
+        try {
+            await ax.put(`/maintenance`, {
+                    id,
+                    item,
+                    price,
+                    mileage,
+                    company,
+                    memo,
+                    date,
+                }, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     const handleGoBack = () => {
         navigate(-1);
@@ -39,26 +91,26 @@ export default function MaintenanceHistoryId() {
             </FlexDiv>
             <FlexDiv className="flex-col gap-2">
                 <Label htmlFor="item" className="text-sm font-semibold">항목</Label>
-                <Input id="item" placeholder="항목을 입력해주세요" />
+                <Input id="item" value={item} onChange={(e) => setItem(e.target.value)} placeholder="항목을 입력해주세요" />
             </FlexDiv>
             <FlexDiv className="flex-col gap-2">
                 <Label htmlFor="price" className="text-sm font-semibold">금액</Label>
-                <Input id="price" placeholder="0원" />
+                <Input id="price" value={price} onChange={(e) => setPrice(e.target.value)} placeholder="0원" />
             </FlexDiv>
             <FlexDiv className="flex-col gap-2">
                 <Label htmlFor="kilometer" className="text-sm font-semibold">키로수</Label>
-                <Input id="kilometer" placeholder="0km" />
+                <Input id="kilometer" value={mileage} onChange={(e) => setMileage(e.target.value)} placeholder="0km" />
             </FlexDiv>
             <FlexDiv className="flex-col gap-2">
                 <Label htmlFor="company" className="text-sm font-semibold">업체명</Label>
-                <Input id="company" placeholder="업체명을 입력해주세요" />
+                <Input id="company" value={company} onChange={(e) => setCompany(e.target.value)} placeholder="업체명을 입력해주세요" />
             </FlexDiv>
             <FlexDiv className="flex-col gap-2">
                 <Label htmlFor="memo" className="text-sm font-semibold">메모</Label>
-                <Textarea id="memo" className="bg-white" />
+                <Textarea id="memo" value={memo} onChange={(e) => setMemo(e.target.value)} className="bg-white" />
             </FlexDiv>
             <FlexDiv className="justify-end gap-2">
-                <Button variant="default"><Link to="/management/maintenance-history">저장</Link></Button>
+                <Button variant="default" onClick={() => handleUpdate()}>저장</Button>
                 <Button variant="outline" onClick={handleGoBack}>취소</Button>
             </FlexDiv>
         </FlexDiv>
